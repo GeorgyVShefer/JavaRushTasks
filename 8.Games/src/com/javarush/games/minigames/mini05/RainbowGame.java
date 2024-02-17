57cc28c4afb696458606ce53ecf68359bcb1dccc
GeorgyVShefer <an.yan2369@gmail.com>
2024-02-17 22:00:35 +0300

    Task1711

--- 8.Games/src/com/javarush/games/minigames/mini05/RainbowGame.java
+++ 8.Games/src/com/javarush/games/minigames/mini05/RainbowGame.java
@@ -9,14 +9,17 @@ import com.javarush.engine.cell.Color;
 
 public class RainbowGame extends Game {
 
+    //напишите тут ваш код
+
     @Override
     public void initialize() {
-        setScreenSize(10, 7);
-        Color[] colors = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.INDIGO, Color.VIOLET};
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 7; y++) {
-                setCellColor(x, y, colors[y]);
-            }
-        }
+        setScreenSize(10,7);
+        setCellColor(0,0,Color.RED);
+        setCellColor(0,1,Color.ORANGE);
+        setCellColor(0,2,Color.YELLOW);
+        setCellColor(0,3,Color.GREEN);
+        setCellColor(0,4,Color.BLUE);
+        setCellColor(0,5,Color.INDIGO);
+        setCellColor(0,6,Color.VIOLET);
     }
 }
